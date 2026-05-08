301fd5a2315a9ade0f0332b1419a527c38561f15
Cleytonrik99 <enriquecleyton@gmail.com>
2026-05-08 10:21:37 -0300

    Healthcare Providers Controller in progress

---
+++ MedSave/Controllers/HealthcareProvidersController.cs
@@ -0,0 +1,106 @@
+﻿using MedSave.DTOs.HealthcareProviders;
+using MedSave.Services.HealthcareProviders;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedSave.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthcareProvidersController : ControllerBase
+{
+    private readonly IHealthcareProvidersService _healthcareProvidersService;
+
+    public HealthcareProvidersController(IHealthcareProvidersService healthcareProvidersService)
+    {
+        _healthcareProvidersService = healthcareProvidersService;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetAll()
+    {
+        try
+        {
+            var dtos = await _healthcareProvidersService.GetAllAsync();
+
+            return Ok(new
+            {
+                items = dtos,
+                pageInfo = new
+                {
+                    page = 1,
+                    pageSize = dtos.Count(),
+                    totalItems = dtos.Count(),
+                    totalPages = 1
+                }
+            });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new
+            {
+                message = "Internal error when searching for the Healthcare Providers",
+                details = ex.Message
+            });
+        }
+    }
+
+    [HttpGet("{id:long}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetById(long id)
+    {
+        try
+        {
+            var dto = await _healthcareProvidersService.GetByIdAsync(id);
+
+            return Ok(dto);
+        }
+        catch (HealthcareProvidersService.NotFoundException ex)
+        {
+            return StatusCode(404, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new
+            {
+                message = "Internal error when searching for the Healthcare Provider",
+                details = ex.Message
+            });
+        }
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> AddHealthcareProviders([FromBody] CreateHealthcareProviderRequest req)
+    {
+        try
+        {
+            var created = await _healthcareProvidersService.AddAsync(req.HealthcareProvidersDto, req.AddressStockDto,
+                req.ProviderTypeDto);
+
+            return CreatedAtAction(nameof(GetById), new { id = created.HealthcareProviderId }, created);
+        }
+        catch (ArgumentNullException ex)
+        {
+            return BadRequest(new { message = "Invalid request body." });
+        }
+        catch (HealthcareProvidersService.ConflictException ex)
+        {
+            return StatusCode(409, new { message = "" });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new
+            {
+                message = "Internal error when adding the Healthcare Provider",
+                details = ex.Message
+            });
+        }
+    }
+}
