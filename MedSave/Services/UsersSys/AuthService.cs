c3069fbdd06d83ff12f2b83f6fc4bba36702d513
Cleytonrik99 <enriquecleyton@gmail.com>
2026-05-21 23:21:47 -0300

    Auth service, jwt Audience returned

--- MedSave/Services/UsersSys/AuthService.cs
+++ MedSave/Services/UsersSys/AuthService.cs
@@ -52,6 +52,8 @@ namespace MedSave.Services.Auth
         private string GenerateJwtToken(Model.UsersSys user)
         {
             var jwtKey = _configuration["Jwt:Key"];
+            var jwtIssuer = _configuration["Jwt:Issuer"];
+            var jwtAudience = _configuration["Jwt:Audience"];
 
             var securityKey = new SymmetricSecurityKey(
                 Encoding.UTF8.GetBytes(jwtKey!)
@@ -66,16 +68,16 @@ namespace MedSave.Services.Auth
             {
                 new Claim("userId", user.UserId.ToString()),
                 new Claim(JwtRegisteredClaimNames.Sub, user.Email),
-                new Claim(
-                    JwtRegisteredClaimNames.Iat,
+                new Claim(JwtRegisteredClaimNames.Iat,
                     DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString(),
-                    ClaimValueTypes.Integer64
-                )
+                    ClaimValueTypes.Integer64)
             };
 
             var token = new JwtSecurityToken(
+                issuer: jwtIssuer,
+                audience: jwtAudience,
                 claims: claims,
-                expires: DateTime.UtcNow.AddHours(2),
+                expires: DateTime.Now.AddHours(2),
                 signingCredentials: credentials
             );
 
